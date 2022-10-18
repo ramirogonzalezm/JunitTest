815b5cc3332eeb45d167b46d24d4d28e0c818bcb
Ramiro Gonzalez <rgm20_aries@outlook.com>
2022-10-18 17:46:49 -0500

    Initial commit to develop branch

---
+++ src/main/java/com/desarrollo/devops/JunitProyectApplication.java
@@ -0,0 +1,13 @@
+package com.desarrollo.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitProyectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desarrollo/devops/JunitProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desarrollo.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
