aeade74a029c25e276929e50959ed87cd562ff1b
rgm20_aries <rgm20_aries@outlook.com>
2022-10-18 18:46:33 -0500

    Test4

--- src/main/java/com/desarrollo/devops/JunitProyectApplication.java
+++ src/main/java/com/desarrollo/devops/JunitProyectApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JunitProyectApplication {
 
 	public static void main(String[] args) {	
-	
 		SpringApplication.run(JunitProyectApplication.class, args);
 	}
 
